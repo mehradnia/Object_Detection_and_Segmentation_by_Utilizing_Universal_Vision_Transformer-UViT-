540373b0fe44cf1bb825ce9b41171c959c340cfb
Mohammad Mehradnia <mehradnia.m97@gmail.com>
2024-12-03 12:46:28 +0100

    initiate the main class, load configs and the train and val datasets

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from data_loader.datasets.coco2017 import COCO2017
+from config.config import Config
+
+
+class Main:
+    config_file_path = 'src/config/configs.yaml'
+
+    def __init__(self):
+        pass
+
+    def bootstrap(self):
+        config = Config()
+        config.set_configs(self.config_file_path)
+
+        dataset_configs = config.get_config('datasets.coco2017')
+        model_configs = config.get_config('models.uvit')
+        general_configs = config.get_config('general')
+        data_loader = COCO2017(
+            data_dir=dataset_configs['path'],
+            batch_size=general_configs['batch_size'],
+            image_size=model_configs['image_size']
+        )
+
+        data_loader.set_datasets()
+
+        train_dataset = data_loader.get_train_dataset()
+        val_dataset = data_loader.get_validation_data()
+
+
+if __name__ == '__main__':
+    Main().bootstrap()
